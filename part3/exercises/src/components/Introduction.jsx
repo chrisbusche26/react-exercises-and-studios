e64bc93b379e2ebf6f2c0478567340356e0460fa
chrisbusche26 <chrisbusche26@gmail.com>
2024-03-18 19:30:06 -0500

    class 20 exercises

--- part3/exercises/src/components/Introduction.jsx
+++ part3/exercises/src/components/Introduction.jsx
@@ -1,11 +1,11 @@
 export default function HobbyIntroduction () {
    return(
       <div>
-         <h2>3 Fun Facts About Needlepoint</h2>
+         <h2>3 Fun Facts About Video Games</h2>
          <ol>
-            <li>Needlepoint uses a specialized canvas as it's base.</li>
-            <li>The most basic stitch is called a tent stitch.</li>
-            <li>Needlepoint uses a lot of different kinds of fibers.</li>
+            <li>The first video game was created in 1958.</li>
+            <li>Minecraft has the most copies sold of any game at approx. 300,000,000.</li>
+            <li>Mortal Kombat led to the creation of the ESRB.</li>
          </ol>
       </div>
    );
